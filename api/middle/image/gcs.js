0da45ce25d8bc666172407181217f78c83d2745f
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2018-03-20 16:26:53 +0800

    revise img upload api (#2)

--- api/middle/image/gcs.js
+++ api/middle/image/gcs.js
@@ -12,6 +12,30 @@ const initBucket = (bucket) => {
 	return gcs.bucket(bucket);
 }
 
+const getFileMd5Hash = (bucketFile) => {
+  return new Promise((resolve, reject) => {
+    bucketFile.getMetadata().then(results => {
+      const metadata = results[0]
+      resolve(metadata.md5Hash)
+    }).catch(err => {
+      reject(err)
+    })
+  })
+}
+
+const renameFile = (bucket, srcFilename, destFilename) => {
+  return new Promise((resolve, reject) => {
+    bucket.file(srcFilename)
+    .move(bucket.file(destFilename))
+    .then(() => {
+      resolve(bucket.file(destFilename))
+    })
+    .catch(err => {
+      reject(err)
+    })
+  })
+}
+
 const makeFilePublic = (bucketFile) => {
 	return new Promise((resolve, reject) => {
 		bucketFile.makePublic()
@@ -33,10 +57,10 @@ const uploadFileToBucket = (bucket, filePath, options) => {
     if (opts.cacheControl) {
         metadata.cacheControl = opts.cacheControl
     }
-    ('Uploading image file...')
+
 		bucket.upload(filePath, options)
 		.then(() => { resolve(bucketFile) })
-		.catch(err => reject(err))
+		.catch(err => { reject(err) })
 	})
 }
 
@@ -49,6 +73,18 @@ const deleteFileFromBucket = (bucket, options) => {
 		.then(() => { resolve(bucketFile) })
 		.catch(err => { reject(err) })
 	})
+}
+
+const deleteFilesInFolder = (bucket, options) => {
+  return new Promise((resolve, reject) => {
+    const opts = options || {}
+    bucket.deleteFiles({
+      prefix: opts.folder,
+      force: true
+    })
+		.then(() => { resolve() })
+		.catch(err => { reject(err) })
+  })
 }
 
 const publishAction = (data) => {
@@ -75,9 +111,12 @@ const publishAction = (data) => {
 }
 
 module.exports = {
-	initBucket,
+  initBucket,
+  getFileMd5Hash,
+  renameFile,
 	makeFilePublic,
   uploadFileToBucket,
   deleteFileFromBucket,
+  deleteFilesInFolder,
   publishAction
 }
